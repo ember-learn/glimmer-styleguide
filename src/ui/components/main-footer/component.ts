a9b18365e950cc00e448513a64200842a571237e
Ricardo Mendes <rokusu@gmail.com>
2017-06-23 10:03:25 +0100

    Moves sponsor logos into repository.
    When I figure out how to import ENV, I'll change `assetPath` so it uses a local path when developing so you can work offline.

--- src/ui/components/main-footer/component.ts
+++ src/ui/components/main-footer/component.ts
@@ -1,7 +1,13 @@
 import Component from '@glimmer/component';
+// import ENV from '../../../../config/environment';
 
 export default class MainFooter extends Component {
     get currentYear() {
         return new Date().getUTCFullYear();
+    }
+
+    get assetPath() {
+        // return ENV.assetPath;
+        return 'https://glimmer-styleguide.global.ssl.fastly.net/glimmer-styleguide/master';
     }
 };
